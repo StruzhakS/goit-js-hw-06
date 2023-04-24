46720d757ff29e399f366b69aee3962bad172ef6
StruzhakS <StruzhakS@gmail.com>
2023-04-24 14:09:00 +0300

    Почав домашку)

---
+++ js/myjs.js
@@ -0,0 +1,6 @@
+const category = document.querySelector('#categories')
+// console.dir(`Number of categories: ${category.children.length}`);
+
+const title = document.querySelectorAll('h2')
+console.log(title[0]);
+// console.log(`Category: ${title[0].textContent} Elements: `);
